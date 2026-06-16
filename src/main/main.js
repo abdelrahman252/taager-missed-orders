9a4799dd63a6e4c979e7c30d1e608141a3e61237
Abdelrahman <abdelrahmanahmed.3456@gmail.com>
2026-06-16 03:26:48 +0300

    chore: release v1.0.43 with updated AppUserModelId to force taskbar icon cache reload

--- src/main/main.js
+++ src/main/main.js
@@ -14,7 +14,7 @@
 })();
 
 const { app, BrowserWindow, ipcMain, dialog, shell, Tray, Menu, nativeImage } = require("electron");
-app.setAppUserModelId("com.taagerbot.app");
+app.setAppUserModelId("com.taagerbot.orders");
 const path = require("path");
 const Store = require("electron-store");
 const fs = require("fs");
